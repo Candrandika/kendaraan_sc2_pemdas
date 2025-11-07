65884d029cd048db66c9d749f6a9882f53a1a80d
Candra Andika Putra <candrandikarpl1.kanesa@gmail.com>
2025-11-07 07:49:30 +0700

    tambah class abstract

---
+++ Kendaraan.java
@@ -0,0 +1,29 @@
+package kendaraan_sc2_pemdas;
+
+abstract class Kendaraan {
+    private String id;
+    private String nama;
+    private double biayaHarian;
+    private boolean tersedia;
+
+    public Kendaraan(String id, String nama, double biayaHarian, boolean tersedia) {
+        this.id = id;
+        this.nama = nama;
+        this.biayaHarian = biayaHarian;
+        this.tersedia = tersedia;
+    }
+
+    public String getId()  {return id;}
+    public String getNama() {return nama;}
+    public double getBiayaHarian() {return biayaHarian;}
+    public boolean isTersedia() {return tersedia;}
+    public double hitungBiayaTotal(int hari) {
+        if(hari <= 0) throw new IllegalArgumentException("Hari sewa harus positif");
+        return biayaHarian * hari + this.hitungBiayaTambahan();
+    }
+    public void tampilkanDetail() {
+        System.out.printf("ID: %s, Nama: %s, Biaya Harian: Rp %f, Status: %s\n", this.id, this.nama, this.biayaHarian, this.tersedia ? "Tersedia" : "Tidak Tersedia");
+    }
+
+    protected abstract double hitungBiayaTambahan();
+}
